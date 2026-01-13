1bda9d795c9d6398445f7f855f013cc30ccc35d1
Jordan Lambert <jordan.lmb1@gmail.com>
2026-01-13 17:56:53 -0500

    chore: configure rewrites for CardValue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/CardValue",
+        destination: "https://card-value-ashen.vercel.app",
+      },
+      {
+        source: "/CardValue/:path*",
+        destination: "https://card-value-ashen.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
